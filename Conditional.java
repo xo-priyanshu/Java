fc852daeed95a0c2b391eddadcaf7424fc66b7e1
Priyanshu Dubey <priyansudubey36@gmail.com>
2025-09-30 23:08:44 +0530

    Initial commit

---
+++ Conditional.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Conditional {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a==b) {
+            System.out.println("equal");
+        }
+        else{
+            if (a>b){
+                System.out.println("a is greater");
+            }
+        
+        else {
+            System.out.println("a is lesser");
+        }
+    }
+}
